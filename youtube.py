14b2f8bc01b71ca000ab659939f83f134b599634
pusky <vale.ticu2000@gmail.com>
2021-08-11 21:09:54 +0200

    youtube_dl

--- youtube.py
+++ youtube.py
@@ -1,16 +1,1 @@
-from pytube import YouTube
-
-class Song:
-    def __init__(song, link):
-        song.yt = YouTube(link,on_progress_callback=progress_func,
-                          on_complete_callback=complete_func,
-                          proxies=my_proxies)
-
-    def getTitle(song):
-        print(song.yt.title)
-
-    def getThumb(song):
-        print(song.yt.thumbnail_url)
-
-song = Song('https://www.youtube.com/watch?v=WLnnMOLiFtg')
-song.getTitle()
+import youtube_dl
